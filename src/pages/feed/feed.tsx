db487d37680b7cb8603a42127241a29dc36427da
Mikhail Rodichev <rodichevmichael@gmail.com>
2025-09-13 10:39:37 +0300

    Replace static placeholders in `Feed` and `FeedInfo` components with data from Redux store and integrate `fetchFeed` action for dynamic updates.

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,27 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { RootState, useDispatch, useSelector } from '../../services/store';
+import { fetchFeed } from '../../services/slices/feedSlice';
 
 export const Feed: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
-
-  if (!orders.length) {
-    return <Preloader />;
-  }
-
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  const dispatch = useDispatch();
+  const { isLoading, data } = useSelector((state: RootState) => state.feed);
+  useEffect(() => {
+    dispatch(fetchFeed());
+  }, [dispatch]);
+  return (
+    <>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <FeedUI
+          orders={data.orders}
+          handleGetFeeds={() => {
+            dispatch(fetchFeed());
+          }}
+        />
+      )}
+    </>
+  );
 };
